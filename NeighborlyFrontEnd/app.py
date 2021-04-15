13d64815004ab43a4a49a306f914353e884b7a0b
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-04-15 14:38:01 -0500

    Update app.py

--- NeighborlyFrontEnd/app.py
+++ NeighborlyFrontEnd/app.py
@@ -70,18 +70,23 @@ def home():
     posts = response2.json()
     return render_template("index.html", ads=ads, posts=posts)
 
-
 @app.route('/ad/add', methods=['GET'])
 def add_ad_view():
     return render_template("new_ad.html")
 
-
-@app.route('/ad/edit/<id>', methods=['GET'])
-def edit_ad_view(id):
-    response = requests.get(settings.API_URL + '/getAdvertisement?id=' + id)
-    ad = response.json()
-    return render_template("edit_ad.html", ad=ad)
-
+@app.route('/ad/new', methods=['POST'])
+def add_ad_request():
+    # Get item from the POST body
+    req_data = {
+        "title": request.form["title"],
+        "city": request.form["city"],
+        "description": request.form["description"],
+        "email": request.form["email"],
+        "imgUrl": request.form["imgUrl"],
+        "price": request.form["price"]
+    }
+    response = requests.post(settings.API_URL + '/createAdvertisement', json=req_data)
+    return redirect(url_for('home'))
 
 @app.route('/ad/delete/<id>', methods=['GET'])
 def delete_ad_view(id):
@@ -95,21 +100,11 @@ def view_ad_view(id):
     ad = response.json()
     return render_template("view_ad.html", ad=ad)
 
-@app.route('/ad/new', methods=['POST'])
-def add_ad_request():
-    # Get item from the POST body
-    req_data = {
-        "title": request.form["title"],
-        "city": request.form["city"],
-        "description": request.form["description"],
-        "email": request.form["email"],
-        "imgUrl": request.form["imgUrl"],
-        "price": request.form["price"]
-    }
-    print(req_data)
-    response = requests.post(settings.API_URL + '/createAdvertisement', json=req_data)
-    print(response)
-    return redirect(url_for('home'))
+@app.route('/ad/edit/<id>', methods=['GET'])
+def edit_ad_view(id):
+    response = requests.get(settings.API_URL + '/getAdvertisement?id=' + id)
+    ad = response.json()
+    return render_template("edit_ad.html", ad=ad)
 
 @app.route('/ad/update/<id>', methods=['POST'])
 def update_ad_request(id):
@@ -122,12 +117,12 @@ def update_ad_request(id):
         "imgUrl": request.form["imgUrl"],
         "price": request.form["price"]
     }
-    print(req_data)
     response = requests.put(settings.API_URL + '/updateAdvertisement?id=' + id, json=req_data)
     return redirect(url_for('home'))
 
 @app.route('/ad/delete/<id>', methods=['POST'])
 def delete_ad_request(id):
+    print(settings.API_URL + '/deleteAdvertisement?id=' + id)
     response = requests.delete(settings.API_URL + '/deleteAdvertisement?id=' + id)
     if response.status_code == 200:
         return redirect(url_for('home'))
