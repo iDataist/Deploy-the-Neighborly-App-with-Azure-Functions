e7d7458ec5a27b0b46237db159af119dda35aa41
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-04-15 10:14:55 -0500

    Update settings.py

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -16,4 +16,4 @@ API_VERSION = 'v1'
 # API_URL = "https://funcapp20210411.azurewebsites.net/api"
 
 # for local host if Azure functions served locally
-API_URL = "http://localhost:8000/api"
+API_URL = "http://localhost:7071/api"
